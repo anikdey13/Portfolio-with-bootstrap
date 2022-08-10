8cc7a2dc4396c69bfaf54769481d0ea248d9327d
Anik <deyanik659@gmail.com>
2022-08-10 22:07:33 +0600

    1st commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+// {
+//     let number = document.getElementById('number');
+//     let circle = document.getElementsByClassName('circle');
+
+//     if(number.innerHTML == 100){
+//         circle.style.animation = "anim1 2s linear forwards";
+//     }
+//     else if(number.innerHTML == 70){
+//         circle.style.animation = "anim2 2s linear forwards";
+//     }
+
+//     else if(number.innerHTML == 30){
+//         circle.style.animation = "anim3 2s linear forwards";
+//     }
+//     else if(number.innerHTML == 10){
+//         circle.style.animation = "anim4 2s linear forwards";
+//     }
+//     else{
+
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+// let number = document.getElementById('number');
+
+// let counter = 0;
+
+// setInterval(() =>{
+//     if(counter == 100){
+//         clearInterval();
+//     }else{
+//         counter+= 1;
+//         number.innerHTML = counter + "%" ;
+//     }
+
+// },10)}
